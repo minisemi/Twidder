7e7e9767b56fd59ee1e508e41e5c8f3651d7e8d1
matso354 <matso354@student.liu.se>
2017-02-20 14:43:45 +0100

    new repo structure lab 3

---
+++ Twidder/Twidder/database_helper.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+import sqlite3
+from flask import Flask
+from flask import g
+import uuid
+import json
+
+DATABASE = 'database.db'
+
+app = Flask(__name__)
+
+def find_user(email):
+    user = query_db("SELECT email, firstName, familyName, gender, city, country FROM Users WHERE email=?", [email], one=True)
+    if user is None:
+        return None
+    return user
+
+def sign_out_user(user):
+    query = "DELETE FROM ActiveUsers WHERE user=?"
+    query_db(query, [user], one=True)
+
+def sign_up_user(email, firstName, familyName, password, gender, city, country):
+
+
+    query = "INSERT INTO Users VALUES (?,?,?,?,?,?,?)"          #Do we need to specify the database?
+    query_db(query, [email, firstName, familyName, password, gender, city, country], one=True)
+
+def remove_user(email):
+    query = "IF  EXISTS (SELECT * FROM Users WHERE email=?) DELETE FROM Users WHERE email=?"
+    query_db(query, [email], one=True)
+    if query_db(query, [email], one=True) is None:
+        return None
+    return "user removed"
+
+def get_posts(email):
+    query = "SELECT message FROM Posts WHERE receiver=?"
+    return query_db(query, [email])
+
+def create_post(sender, receiver, message):
+    query = "INSERT INTO Posts VALUES (?,?,?)"
+    query_db(query, [receiver, sender, message], one=True)
+
+#Checks is user is active
+def check_if_active(token):
+    queryString = "SELECT user FROM ActiveUsers WHERE token=?"
+    user = query_db(queryString, [token], one=True)
+    if user is None:
+        return "NotActive"
+    email = user['user']
+    return email
+
+def check_if_active_email(email):
+    queryString = "SELECT user FROM ActiveUsers WHERE email=?"
+    user = query_db(queryString, [email], one=True)
+    if user is None:
+        return "NotActive"
+    email = user['user']
+    return email
+
+def sign_in(email):
+    if check_if_active_email(email) is not None:
+        sign_out_user(email)
+    query = "INSERT INTO ActiveUsers VALUES ?, ?"
+    token = str(uuid.uuid4().hex)
+    query_db(query, [email, token], one=True)
+
+#Checks if password is correct
+def check_password(password, email):
+    queryString = "SELECT password FROM Users WHERE email=? AND password=?"
+    correctPassword = query_db(queryString, [email, password], one=True)
+    if correctPassword is None:
+        return False
+    return True
+
+def update_password(password, user):
+    queryString = "UPDATE Users SET password=? WHERE user=?"
+    query_db(queryString, [password, user], one=True)
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('database.schema', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+        print "Database initalized"
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+def query_db(query, args=(), one=False):
+    db = get_db()
+    cur = db.execute(query, args)
+    #rv = cur.fetchall()
+    rv = [dict((cur.description[i][0], value) \
+              for i, value in enumerate(row)) for row in cur.fetchall()]
+    db.commit()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+#def make_dicts(cursor, row):
+ #   return dict((cursor.description[idx][0], value)
+  #              for idx, value in enumerate(row))
+
+
---
+++ Twidder/static/client.js
@@ -0,0 +1,183 @@
+window.onload = function () {
+    displayView()
+}
+
+function displayView() {
+    if (sessionStorage.token == undefined){
+        return document.getElementById("currentView").innerHTML = document.getElementById("welcome").innerHTML
+    } else {
+        return document.getElementById("currentView").innerHTML= document.getElementById("profile").innerHTML
+    }
+}
+
+
+function logout(){
+    serverstub.signOut(sessionStorage.token)
+    sessionStorage.removeItem("token")
+    sessionStorage.removeItem("email")
+    displayView()
+}
+
+function login() {
+    var error = document.getElementById("error")
+    var serverMessage = serverstub.signIn(document.getElementById("loginemail").value, document.getElementById("loginpassword").value)
+        if (serverMessage.success == true) {
+            checkForToken(serverMessage.data, document.getElementById("loginemail").value)
+        } else {
+            error.innerHTML = serverMessage.message
+        }
+}
+
+function signUp() {
+
+    var proceed = true
+    var error = document.getElementById("error")
+
+
+    if(document.getElementById("password").value!=document.getElementById("repeatpsw").value){
+        error.innerHTML = "Repeat password failed"
+        proceed = false
+    }
+
+    var select = document.getElementById("gender")
+
+    if (proceed) {
+        var email = document.getElementById("email").value
+        var password = document.getElementById("password").value
+        var firstname = document.getElementById("firstname").value
+        var familyname = document.getElementById("familyname").value
+        var gender = select.options[select.selectedIndex].text
+        var city = document.getElementById("city").value
+        var country = document.getElementById("country").value
+        var user = {
+            email: email,
+            password: password,
+            firstname: firstname,
+            familyname: familyname,
+            gender: gender,
+            city: city,
+            country: country
+        }
+        // TODO: Useful in lab 3
+        // data = FormData();
+        // data.append("email", email)
+        var serverMessage = serverstub.signUp(user)
+        if (serverMessage.success == true) {
+            error.innerHTML = serverMessage.message
+        } else {
+            error.innerHTML = serverMessage.message
+        }
+    }
+}
+
+function checkForToken(token, email){
+    if (typeof(Storage) !== "undefined") {
+        sessionStorage.setItem("token", token)
+        sessionStorage.setItem("email", email)
+        displayView()
+    } else {
+      //  alert("Browser doesn't support web storage")
+    }
+}
+
+function openTab(tabName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablinks");
+
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    event.currentTarget.className += " active";
+
+}
+
+function changePassword() {
+    var pwText = document.getElementById("passwordText")
+    var oldpw =document.getElementById("changePasswordOld").value
+    var newpw1=document.getElementById("changePasswordNew1").value
+    var newpw2=document.getElementById("changePasswordNew2").value
+    if(newpw1!=newpw2){
+        pwText.innerHTML = "Repeat password failed"
+        return false
+    }
+
+    if (newpw1!=oldpw){
+        var serverMessage = serverstub.changePassword(sessionStorage.token,oldpw,newpw1)
+        if(serverMessage.success==true){
+        pwText.innerHTML="Password successfully changed"
+        } else{
+            pwText.innerHTML="Wrong password"
+        }
+    } else {
+        pwText.innerHTML="New password same as old"
+    }
+    return false
+}
+
+function displayUserData(){
+    var serverMessage=serverstub.getUserDataByToken(sessionStorage.token)
+    document.getElementById("userfirstname").innerHTML=serverMessage.data.firstname
+    document.getElementById("userfamilyname").innerHTML=serverMessage.data.familyname
+    document.getElementById("useremail").innerHTML=serverMessage.data.email
+    document.getElementById("usergender").innerHTML=serverMessage.data.gender
+    document.getElementById("usercountry").innerHTML=serverMessage.data.country
+    document.getElementById("usercity").innerHTML=serverMessage.data.city
+}
+
+function postMessage(message, email, messageBoard) {
+    var message = document.getElementById(message).value
+    var serverMessage=serverstub.postMessage(sessionStorage.token,message,email)
+    var messageBoard = document.getElementById(messageBoard)
+    var textArea = document.createElement("textarea")
+    textArea.setAttribute("readonly","readonly")
+
+    var textNode = document.createTextNode(message)
+    textArea.appendChild(textNode)
+    messageBoard.insertBefore(textArea, messageBoard.firstChild);
+}
+
+function refreshMessages(email, messageBoard) {
+    var messages = serverstub.getUserMessagesByEmail(sessionStorage.token, email)
+    var messageBoard = document.getElementById(messageBoard)
+    if (messages.data.length>messageBoard.childElementCount){
+        while (messageBoard.firstChild) {
+            messageBoard.removeChild(messageBoard.firstChild);
+        }
+
+        for (var i = 0; i < messages.data.length; i++) {
+            var textArea = document.createElement("textarea")
+            textArea.setAttribute("readonly","readonly")
+
+            var text = messages.data[i].content
+            var textNode = document.createTextNode(text)
+            textArea.appendChild(textNode)
+            messageBoard.appendChild(textArea)
+        }
+    }
+}
+
+function searchForUser() {
+
+    var searchEmail = document.getElementById("searchEmail").value
+    var serverMessage=serverstub.getUserDataByEmail(sessionStorage.token, searchEmail)
+    if (serverMessage.success == true) {
+
+        document.getElementById("searchResults").style.visibility="visible"
+        document.getElementById("searchfirstname").innerHTML = serverMessage.data.firstname
+        document.getElementById("searchfamilyname").innerHTML = serverMessage.data.familyname
+        document.getElementById("searchemail").innerHTML = serverMessage.data.email
+        document.getElementById("searchgender").innerHTML = serverMessage.data.gender
+        document.getElementById("searchcountry").innerHTML = serverMessage.data.country
+        document.getElementById("searchcity").innerHTML = serverMessage.data.city
+        refreshMessages(searchEmail,"searchmessageBoard")
+
+    } else {
+      //  alert(serverMessage.message)
+    }
+}
